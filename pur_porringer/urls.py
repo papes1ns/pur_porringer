55c5176482f7f5774b721bde250533fa9fab1f78
Nathan Papes <you@example.com>
2015-11-15 00:12:12 -0500

    Fix URLs problem

--- pur_porringer/urls.py
+++ pur_porringer/urls.py
@@ -6,5 +6,5 @@ from main.views import ip_broker, index
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'^ip/$', ip_broker, name="ip_broker"),
-    url(r'^', index, name="index"),
+    url(r'^$', index, name="index"),
 ]
