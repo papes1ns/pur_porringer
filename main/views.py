c678c5c8b49e238be38533b976fc475c32671870
Nathan Papes <you@example.com>
2015-11-21 16:29:42 -0500

    Better call to RPi

--- main/views.py
+++ main/views.py
@@ -10,37 +10,30 @@ from .models import Log
 @csrf_exempt
 def index(request):
     context = {}
-    connected = is_connected()
-    if request.method == "POST":
+    client = is_connected()
+    if request.method == "POST" and client != False:
         try:
-            call_motor_on()
+            call_motor_on(client)
             Log.objects.create()
             return HttpResponseRedirect(reverse("index"))
         except Exception, e:
             print e
 
-    context["connected"] = connected
+    context["client"] = client
     context["log"] = Log.objects.all()[:10][::-1]
     return render(request, "index.html", context)
 
 
-def call_motor_on():
-    client = SSHClient()
-    client.load_system_host_keys()
-    client.set_missing_host_key_policy(AutoAddPolicy())
-    client.connect("127.0.0.1", 2222, "pi", "raspberry")
-    stdin, stdout, stderr = client.exec_command("ls ~/Desktop")
-
-    for line in stdout.read().splitlines():
-        print line
+def call_motor_on(client):
+    stdin, stdout, stderr = client.exec_command("sudo . /home/pi/motor_on.py")
 
 def is_connected():
     client = SSHClient()
     client.load_system_host_keys()
     client.set_missing_host_key_policy(AutoAddPolicy())
     try:
-        client.connect("127.0.0.1", 2222, "pi", "raspberry")
-        return True
+        client.connect("127.0.0.1", 2222, "pi", "raspberry", 5)
+        return client
     except Exception, e:
         print e
         return False
